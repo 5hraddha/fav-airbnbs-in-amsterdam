623ad29d1a05b39a432a7a48393e7d0fb5648043
Shraddha <mailmeatshraddha@gmail.com>
2023-05-17 17:40:06 -0400

    update the price range for airbnbs to select

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ dataframe = pd.read_csv(
 )
 
 # We have a limited budget, therefore we would like to exclude
-# listings with a price above 1000 INR per night
-dataframe = dataframe[dataframe["Price"] <= 100]
+# listings with a price above 10000 INR per night
+dataframe = dataframe[dataframe["Price"] <= 10000]
 
 # Display as integer
 dataframe["Airbnb Listing ID"] = dataframe["Airbnb Listing ID"].astype(int)
